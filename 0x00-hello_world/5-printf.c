94e1848904297471204aeea1adf670b7083efa71
Folabadmus1 <pholarbadmus@gmail.com>
2023-03-16 20:49:42 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - for the main function
+ *
+ * Return: 0 when it is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
