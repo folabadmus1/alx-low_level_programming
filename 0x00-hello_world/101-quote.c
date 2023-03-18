05645e3ff05c19f43fefdb948f47345416eaa539
Folabadmus1 <pholarbadmus@gmail.com>
2023-03-17 18:17:21 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - the main fuction
+ * main - A c program that print a line standard error
  *
- * Return: 1
+ * Return: 1(error)
  */
+
 int main(void)
 
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
