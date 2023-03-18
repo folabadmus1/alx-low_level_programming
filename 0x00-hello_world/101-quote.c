f825588bdc0104f9a882098acdca2404934494f9
Folabadmus1 <pholarbadmus@gmail.com>
2023-03-18 15:44:02 -0700

    standard erro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - A c program that print a line standard error
  *
- * Return: 1(error)
+ * Return: 1(successful)
  */
 
 int main(void)
 
 {
-	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
