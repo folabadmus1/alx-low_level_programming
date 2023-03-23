a62a550321ee3c6b7eac1117031ad68b61d00701
Folabadmus1 <pholarbadmus@gmail.com>
2023-03-22 19:26:39 -0700

    printsign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,32 +2,26 @@
 /**
  * print_sign - function that print signs
  *
- * @n: function parameter 
+ * @n: The number to be checked
  *
- * Return: 1 0 and -1
+ * Return: 1 for positiv num, -1 for negative num,or 0 for anything else
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
+		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
