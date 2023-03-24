1cf98245682e9d523833d8681b4ab404074cebb1
Folabadmus1 <pholarbadmus@gmail.com>
2023-03-24 16:52:58 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _isupper - check uppercase character
+ * @s: number to be checked
+ * Return: 1 for uppercase or 0 if otherwise
+ */
+int _isupper(int s)
+{
+	if (s >= 65 && s <= 90)
+	{
+		return (1);
+	}
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+void print_most_numbers(void);
+
+#endif
