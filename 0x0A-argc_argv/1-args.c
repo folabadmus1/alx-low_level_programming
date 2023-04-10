9a36fb4346c5fcea88d55f7a369084dfb5136128
Folabadmus1 <pholarbadmus@gmail.com>
2023-04-10 16:13:38 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,12 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void) argv;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d = %s\n", i, argv[i]);
-	}
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
