4ae7dcb2ffc620535564497b0ab58b6655d8afa4
Folabadmus1 <pholarbadmus@gmail.com>
2023-04-19 20:55:53 -0700

    Calculate

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,8 +18,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (array != NULL && cmp != NULL)
 		{
 			while (i < size)
+
 			{
 				if (cmp(array[i]))
+
 						return (i);
 
 				i++;
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,79 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - Returns the sum of a and b.
+ *
+ * @a: first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always 0 (success)
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the differences of a and b.
+ *
+ * @a: first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always 0 (success)
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of a and b.
+ *
+ * @a: first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always 0 (success)
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the result of division a and b.
+ *
+ * @a: first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always 0 (success)
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Fucntion that returns the remainder.
+ *
+ * @a: first integer
+ *
+ * @b: the second integer
+ *
+ * Return: Always 0 (success)
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
