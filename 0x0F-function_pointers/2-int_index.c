a82a62b28fb571bd73f85c207141bc70674cf25c
Folabadmus1 <pholarbadmus@gmail.com>
2023-04-19 21:50:17 -0700

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,11 +18,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (array != NULL && cmp != NULL)
 		{
 			while (i < size)
-
 			{
 				if (cmp(array[i]))
-
-						return (i);
+					return (i);
 
 				i++;
 			}
